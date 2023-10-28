a094c98d8a9320d6b2a3375b3f3bd993bf2ab768
Pratyush Singh <aries.pratyush@gmail.com>
2023-10-28 15:29:28 +0530

    refactor: segregate swipe card & animation

--- lib/src/main/java/com/github/lib/SwipeableCard.kt
+++ lib/src/main/java/com/github/lib/SwipeableCard.kt
@@ -3,29 +3,20 @@ package com.github.lib
 import androidx.compose.foundation.gestures.Orientation
 import androidx.compose.foundation.gestures.draggable
 import androidx.compose.foundation.gestures.rememberDraggableState
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableFloatStateOf
-import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import kotlin.math.abs
 import kotlin.math.roundToInt
 
@@ -74,50 +65,3 @@ fun SwipeableCard(
     }
 }
 
-
-@Composable
-fun SwipeAnimationScreen() {
-    var currentIndex by remember { mutableIntStateOf(0) }
-    val cardList = listOf(
-        "Card 1 Content",
-        "Card 2 Content",
-        "Card 3 Content"
-    )
-
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
-        if (currentIndex < cardList.size) {
-            SwipeableCard(
-                onSwipeLeft = {
-                    if (currentIndex < cardList.size - 1) {
-                        currentIndex++
-                    }
-                },
-                onSwipeRight = {
-                    if (currentIndex < cardList.size - 1) {
-                        currentIndex++
-                    }
-                },
-                onCardRemoved = {
-                    currentIndex++
-                }
-            ) { modifier ->
-                Card(
-                    modifier = modifier
-                        .fillMaxSize()
-                        .padding(16.dp),
-                    elevation = CardDefaults.cardElevation(8.dp)
-                ) {
-                    Text(text = cardList[currentIndex], modifier = Modifier.padding(16.dp))
-                }
-            }
-        } else {
-            Text(text = "All cards swiped!", fontSize = 20.sp, fontWeight = FontWeight.Bold)
-        }
-    }
-}
